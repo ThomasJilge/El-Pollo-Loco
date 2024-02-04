14eb71115bbc002a38c481e430e686da17ca02a1
ThomasJilge <thomasjilge@gmx.de>
2024-02-04 13:36:03 +0100

    character animation

--- classes/character.class.js
+++ classes/character.class.js
@@ -2,17 +2,30 @@ class Character extends MovableObject {
 
     height = 280;
     y = 155;
+    imagesWalking = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-22.png',
-            'img/2_character_pepe/2_walk/W-23.png',
-            'img/2_character_pepe/2_walk/W-24.png',
-            'img/2_character_pepe/2_walk/W-25.png',
-            'img/2_character_pepe/2_walk/W-26.png'
-        ]);
+        this.loadImages(this.imagesWalking);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval( () => {
+            let i = this.currentImage % this.imagesWalking.length;
+            let path = this.imagesWalking[i];
+            this.img = this.imageCache[path];
+            this.currentImage ++;
+        }, 100);       
     }
 
     jumg() {
