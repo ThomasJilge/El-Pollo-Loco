7d167256a3dd41e9c88ac6311868a6396422f166
ThomasJilge <thomasjilge@gmx.de>
2024-02-04 12:34:32 +0100

    function animate() for cloud animation added

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,6 +1,6 @@
 class Cloud extends MovableObject {
 
-        y = 20;
+        y = 50;
         width = 500;
         height = 250;
 
@@ -8,8 +8,12 @@ class Cloud extends MovableObject {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;
-        this.y = 20;
-        this.width = 500;
-        this.height = 250;
+        this.animate();
+    }
+
+    animate() {
+        setInterval( () => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 }
