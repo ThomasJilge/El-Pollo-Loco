752e18d773a8d7b8d36c3fabb6528366f86cf333
ThomasJilge <thomasjilge@gmx.de>
2024-02-10 17:07:31 +0100

    implement gravity

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,6 +1,6 @@
 class Character extends MovableObject {
 
-    height = 280;
+    height = 180;
     y = 155;
     speed = 10;
 
@@ -18,6 +18,7 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.imagesWalking);
+        this.applyGravity();
         this.animate();
     }
 
