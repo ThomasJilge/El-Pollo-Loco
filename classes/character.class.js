4cbe515f0e40c9308514f3894a263da56350d575
ThomasJilge <thomasjilge@gmx.de>
2024-02-11 16:55:15 +0100

    show jump animation

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,6 +1,6 @@
 class Character extends MovableObject {
 
-    height = 180;
+    height = 280;
     y = 155;
     speed = 10;
 
@@ -12,12 +12,26 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+
+    imagesJumping = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png'
+    ];
+
     world;
     walking_sound = new Audio('audio/running.mp3');
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.imagesWalking);
+        this.loadImages(this.imagesJumping);
         this.applyGravity();
         this.animate();
     }
@@ -41,8 +55,13 @@ class Character extends MovableObject {
 
         setInterval( () => {
 
-            if(this.world.keyboard.right || this.world.keyboard.left) {
-                this.playAnimation(this.imagesWalking);
+            if(this.isAboveGround()) {
+                this.playAnimation(this.imagesJumping);
+            } else {
+
+                if(this.world.keyboard.right || this.world.keyboard.left) {
+                    this.playAnimation(this.imagesWalking);
+                }
             }
         }, 50);       
     }
