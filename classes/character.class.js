50c6c656e5b9da0a816be784d18b25a666634086
ThomasJilge <thomasjilge@gmx.de>
2024-02-11 17:57:30 +0100

    jump function added

--- classes/character.class.js
+++ classes/character.class.js
@@ -50,6 +50,11 @@ class Character extends MovableObject {
                 this.otherDirection = true;
                 this.walking_sound.play();
             }
+
+            if(this.world.keyboard.up) {
+                this.speedY = 20;
+            }
+
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
