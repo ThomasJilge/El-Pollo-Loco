f1c7cc13b1da74338a4594462fc1b88219350867
ThomasJilge <thomasjilge@gmx.de>
2024-02-15 21:02:59 +0100

    statusbar

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -24,16 +17,6 @@ class MovableObject {
 
     isAboveGround() {
         return this.y < 180;
-    }
-
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {
@@ -59,13 +42,7 @@ class MovableObject {
             this.y < mo.y + mo.height;
     }
 
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+    
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
