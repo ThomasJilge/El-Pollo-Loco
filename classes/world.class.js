2914d30fb32c7b54960e0fc8962293f24867525c
ThomasJilge <thomasjilge@gmx.de>
2024-02-16 22:59:18 +0100

    statusbar v3

--- classes/world.class.js
+++ classes/world.class.js
@@ -25,7 +25,7 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if(this.character.isColliding(enemy))  {
                     this.character.hit();
-                    console.log('Collision with Character, energy', this.character.energy);
+                    this.statusBar.setPercentage(this.character.energy);
                 }
             });
         }, 200);
@@ -37,7 +37,11 @@ class World {
         this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
+
+        this.ctx.translate(-this.camera_x, 0);
         this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0);
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
