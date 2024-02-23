cedc14d7425bc2766af5363db6fccc2f63a9941b
ThomasJilge <thomasjilge@gmx.de>
2024-02-23 23:32:15 +0100

    chicken small added

---
+++ classes/chicken-small.class.js
@@ -0,0 +1,30 @@
+class ChickenSmall extends MovableObject {
+    height = 80;
+    width = 70;
+    y = 350;
+
+    imagesWalking = [
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+
+
+    constructor() {
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.loadImages(this.imagesWalking);
+        this.x = 800 + Math.random() * 2000;
+        this.speed = 0.15 + Math.random() * 0.5;
+        this.animate();
+    }
+
+    animate() {
+        setInterval( () => {
+            this.moveLeft();
+        }, 1000 / 60);
+
+        setInterval( () => {
+            this.playAnimation(this.imagesWalking);
+        }, 200);       
+    }
+}
