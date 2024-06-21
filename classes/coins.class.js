ae2ebddae33ad43db70e524f19a2ae4e3cd81d2f
ThomasJilge <thomasjilge@gmx.de>
2024-06-21 17:36:56 +0200

    coins js doc comments

--- classes/coins.class.js
+++ classes/coins.class.js
@@ -1,8 +1,34 @@
 class Coins extends MovableObject {
 
+    /**
+     * The height of the coin
+     * @type {number}
+     */
+
     height = 170;
+
+    /**
+     * The width of the coin
+     * @type {number}
+     */
+
     width = 170;
+
+    /**
+     * The initial y-coordinate of the coin
+     * @type {number}
+     */
+
     y = 100;
+
+    /**
+     * The offset values for collision detection
+     * @type {Object}
+     * @property {number} top - The top offset
+     * @property {number} bottom - The bottom offset
+     * @property {number} left - The left offset
+     * @property {number} right - The right offset
+     */
 
     offset = {
         top: 50,
@@ -16,14 +42,35 @@ class Coins extends MovableObject {
         'img/8_coin/coin_2.png'
     ];
 
+    /**
+     * Creates an instance of Coins.
+     * Loads the initial image and starts the animation.
+     */
+
     constructor() {
         super().loadImage('img/8_coin/coin_1.png');
         this.loadImages(this.imagesCoins);
+
+        /**
+         * The initial x-coordinate of the coin, randomly set between 200 and 2200
+         * @type {number}
+         */
+
         this.x = 200 + Math.random() * 2000;
+
+        /**
+         * The initial y-coordinate of the coin, randomly set between 100 and 300
+         * @type {number}
+         */
+
         this.y = 100 + Math.random() * 200;
         // this.speed = 0.15 + Math.random() * 0.5;
         this.animate();
     }
+
+    /**
+     * Starts the animation for the coin, changing images every 400 milliseconds
+     */
 
     animate() {
         setInterval(() => {
