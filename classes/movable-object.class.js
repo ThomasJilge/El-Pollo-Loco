5ae5ef66784d6f5d42bcfbdeae5e969aa0b2a342
ThomasJilge <thomasjilge@gmx.de>
2024-06-21 20:36:10 +0200

    movable object js doc comments

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -1,14 +1,93 @@
 class MovableObject extends DrawableObject {
+
+    /** 
+     * The speed of the object
+     * @type {number}
+     * @default 0.15
+     */
+
     speed = 0.15;
+
+    /** 
+     * Indicates if the object is moving in the other direction
+     * @type {boolean}
+     * @default false
+     */
+
     otherDirection = false;
+
+    /** 
+     * The vertical speed of the object
+     * @type {number}
+     * @default 0
+     */
+
     speedY = 0;
+
+    /** 
+     * The acceleration due to gravity
+     * @type {number}
+     * @default 2.5
+     */
+
     acceleration = 2.5;
+
+    /** 
+     * The energy level of the object
+     * @type {number}
+     * @default 100
+     */
+
     energy = 100;
+
+    /** 
+     * The timestamp of the last hit
+     * @type {number}
+     * @default 0
+     */
+
     lastHit = 0;
+
+    /** 
+     * The quantity of bottles collected
+     * @type {number}
+     * @default 0
+     */
+
     quantityBottles = 0;
+
+    /** 
+     * The quantity of coins collected
+     * @type {number}
+     * @default 0
+     */
+
     quantityCoins = 0;
+
+    /** 
+     * The Y-axis offset
+     * @type {number}
+     * @default 0
+     */
+
     offsetY = 0;
+
+    /** 
+     * The X-axis offset
+     * @type {number}
+     * @default 0
+     */
+
     offsetX = 0;
+
+    /**
+     * The offset boundaries of the object
+     * @type {Object}
+     * @property {number} top - Top offset
+     * @property {number} bottom - Bottom offset
+     * @property {number} left - Left offset
+     * @property {number} right - Right offset
+     */
 
     offset = {
         top: 0,
@@ -17,7 +96,9 @@ class MovableObject extends DrawableObject {
         right: 0,
     };
 
-    /** */
+    /** 
+     * Applies gravity to the object
+     */
 
     applyGravity() {
         setInterval(() => {
@@ -28,7 +109,10 @@ class MovableObject extends DrawableObject {
         }, 1000 / 25);
     }
 
-    /** */
+    /** 
+     * Checks if the object is above ground.
+     * @returns {boolean} True if the object is above ground, false otherwise
+     */
 
     isAboveGround() {
         if(this instanceof ThrowableObject) {
@@ -54,7 +138,11 @@ class MovableObject extends DrawableObject {
     //         this.y < mo.y + mo.height;
     // }
 
-    /** */
+    /** 
+     * Checks if the object is colliding with another object
+     * @param {Object} mo - The object to check collision with
+     * @returns {boolean} True if the objects are colliding, false otherwise
+     */
 
     isColliding(mo) {
         return this.x + this.width - this.offset.right >= mo.x + mo.offset.left &&
@@ -79,7 +167,9 @@ class MovableObject extends DrawableObject {
 
     // }
 
-    /** */
+    /** 
+     * Collects bottles and updates the quantity
+     */
 
     collectingBottles() {
         this.quantityBottles += 20;
@@ -88,7 +178,9 @@ class MovableObject extends DrawableObject {
         }
     }
 
-    /** */
+    /** 
+     * Collects coins and updates the quantity
+     */
 
     collectingCoins() {
         this.quantityCoins += 20;
@@ -97,7 +189,10 @@ class MovableObject extends DrawableObject {
         }
     }
 
-    /** */
+    /** 
+     * Plays the animation from the provided images
+     * @param {Array} images - The array of images for the animation
+     */
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
@@ -106,25 +201,33 @@ class MovableObject extends DrawableObject {
         this.currentImage ++;
     }
 
-    /** */
+    /** 
+     * Moves the object to the right
+     */
 
     moveRight() {
         this.x += this.speed;
     }
 
-    /** */
+    /** 
+     * Moves the object to the left
+     */
 
     moveLeft() {
         this.x -= this.speed;    
     }
 
-    /** */
+    /** 
+     * Makes the object jump
+     */
 
     jump() {
         this.speedY = 30;
     }
 
-    /** */
+    /** 
+     * Decreases the energy of the object when hit
+     */
 
     hit() {
         this.energy -= 5;
@@ -135,27 +238,39 @@ class MovableObject extends DrawableObject {
         }
     }
 
-    /** */
+    /** 
+     * Checks if the object is hurt
+     * @returns {boolean} True if the object is hurt, false otherwise
+     */
 
     isHurt() {
-        let timepassed = new Date().getTime() - this.lastHit; // Difference in ms
-        timepassed = timepassed / 1000; // Differnce in s
+        let timepassed = new Date().getTime() - this.lastHit; 
+        timepassed = timepassed / 1000; 
         return timepassed < 1;
     }
 
-    /** */
+    /** 
+     * Checks if the object is dead
+     * @returns {boolean} True if the object is dead, false otherwise
+     */
 
     isDead() {
         return this.energy == 0;
     }
 
-    /** */
+    /** 
+     * Sets the object to idle state
+     * @returns {boolean} True if the object is idle, false otherwise
+     */
 
     isIdle() {
         return this.characterIdle = true;
     }
 
-    /** */
+    /** 
+     * Sets the object to long idle state
+     * @returns {boolean} True if the object is in long idle state, false otherwise
+     */
 
     isLongIdle() {
         return this.characterLongIdle = true;
