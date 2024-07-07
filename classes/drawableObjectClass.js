443c071d2d84b880b8fee377c29d7e9a1ab6df1c
ThomasJilge <thomasjilge@gmx.de>
2024-07-07 20:11:19 +0200

    frame stroke removed

--- classes/drawableObjectClass.js
+++ classes/drawableObjectClass.js
@@ -77,10 +77,10 @@ class DrawableObject {
     drawFrame(ctx) {
         if(this instanceof Character || this instanceof Chicken || this instanceof Bottle || this instanceof Coins || this instanceof ChickenSmall || this instanceof Endboss) {
             ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
+            // ctx.lineWidth = '5';
+            // ctx.strokeStyle = 'blue';
             ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
+            // ctx.stroke();
         }
     }
 
@@ -92,15 +92,15 @@ class DrawableObject {
     drawFrameOffset(ctx) {
         if (this instanceof Character || this instanceof Chicken || this instanceof Bottle || this instanceof Coins || this instanceof ChickenSmall || this instanceof Endboss) {
             ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'red';
+            // ctx.lineWidth = '5';
+            // ctx.strokeStyle = 'red';
             ctx.rect(
                 this.x + this.offset.left,
                 this.y + this.offset.top,
                 this.width - this.offset.left - this.offset.right,
                 this.height - this.offset.top - this.offset.bottom
             );
-            ctx.stroke();
+            // ctx.stroke();
         }
     }
 
